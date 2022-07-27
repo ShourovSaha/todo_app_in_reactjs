8562a98e8dbcdc8bed6784edeb44fb357ca1dcec
Shourov Saha <shourovsaha94@gmail.com>
2022-07-27 12:37:40 +0600

    added todoCreator component, but code is not complete yet.

--- src/todoListTable.js
+++ src/todoListTable.js
@@ -1,7 +1,12 @@
 import { Component } from "react";
 
-export class TodoListTable extends Component{
- render = () => {
-    
- }
+export class TodoListTable extends Component {
+    render = () => {
+        return (<tr>
+            <td>{this.props.item.action}</td>
+            <td>
+                <input type="checkbox" checked={this.props.item.isDone} onChange={() => this.props.callback(this.props.item)} />
+            </td>
+        </tr>);
+    }   
 }
